ffd7dee81231192bfa0f429b131f9abd0689db6e
z86haya <z86haya@gmail.com>
2022-03-28 13:35:13 +0300

    Create study_pyton.py

---
+++ study_pyton.py
@@ -0,0 +1,28 @@
+def annou(f):
+    def wr():
+        print("run")
+        f()
+        print("dpne")
+    return wr
+    
+@annou   
+def helo():
+    print("hrello")
+    
+peop = [
+    {"name":"zlata", "age":6},
+    {"name":"masa", "age":12}
+]
+
+def f(pers):
+        return pers["name"]
+
+peop.sort(key = f)
+print(peop)
+
+
+def squre(x,y):
+    return x*y
+
+#hello
+print(squre(2,4))
